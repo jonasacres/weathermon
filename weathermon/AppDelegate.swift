0c5ed79b6a546fbe6bd670483f1c00710ba8ba7e
Jonas Acres <acresjonas@gmail.com>
2017-05-12 19:09:43 -0700

    Initial Commit

---
+++ weathermon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  weathermon
+//
+//  Created by jonas on 5/12/17.
+//  Copyright © 2017 jonas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ weathermon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  weathermon
+//
+//  Created by jonas on 5/12/17.
+//  Copyright © 2017 jonas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
